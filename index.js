67222634e0e4afc77c60b04a84c7a0a3b961bd33
Matt Stine <mstine@pivotal.io>
2017-03-01 15:10:24 -0600

    change function name to match deployment name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
  */
-exports.helloWorld = function helloWorld (req, res) {
+exports.helloHttp = function helloWorld (req, res) {
   if (req.body.message === undefined) {
     // This is an error case, as "message" is required
     res.status(400).send('No message defined!');
