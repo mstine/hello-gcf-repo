d15318c4c711eaf49a467cd7a8c981449b8c6066
Matt Stine <mstine@pivotal.io>
2017-04-22 16:39:19 -0400

    make a simple change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ exports.helloHttp = function helloWorld (req, res) {
     res.status(400).send('No message defined!');
   } else {
     // Everything is ok
-    console.log(req.body.message);
+    console.log('CHANGE: '+req.body.message);
     res.status(200).end();
   }
 };
